158763c6dcb61c8fee76fa3bbb6292e557e5aa1c
Brett Bowman <bbowman@pacificbiosciences.com>
2013-12-28 13:36:37 -0800

    Removed nucmer duplicate

--- src/pbmgx/nucmer/coverage.py
+++ src/pbmgx/nucmer/coverage.py
@@ -10,21 +10,22 @@ MIN_LEN = 10000
 def calculate_coverage( coord_file, query_fasta, ref_fasta, min_length=MIN_LEN ):
     query_lengths = read_fasta_lengths( query_fasta )
     ref_lengths = read_fasta_lengths( ref_fasta )
-    ref_windows = read_ref_windows( coord_file, min_len )
+    ref_windows = read_ref_windows( coord_file, min_length )
     print ref_windows
     print query_lengths
     print ref_lengths
     combine_windows( ref_windows )
 
-def combine_windows( windows ):
-    for i, wi in enumerate(windows):
-        for wj in windows[i+1:]:
-            print wi, wj
+def combine_windows( window_dict ):
+    for reference, windows in window_dict.iteritems():
+        for i, wi in enumerate(windows):
+            for wj in windows[i+1:]:
+                print wi, wj
 
-def read_ref_windows( coord_file, min_len=MIN_LEN ):
+def read_ref_windows( coord_file, min_length=MIN_LEN ):
     windows = {}
     for hit in NucmerCoordReader( coord_file ):
-        if abs(hit.S1 - hit.E1) < MIN_LEN:
+        if abs(hit.S1 - hit.E1) < min_length:
             continue
         try:
             windows[hit.reference].append( (hit.S1, hit.E1) )

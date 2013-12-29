3a8dc45523aff2d544c2ad323a0cd051e364afe7
Brett Bowman <bbowman@pacificbiosciences.com>
2013-12-29 12:49:58 -0800

    First working version with Gap identification and counting

--- src/pbmgx/nucmer/coverage.py
+++ src/pbmgx/nucmer/coverage.py
@@ -3,6 +3,7 @@
 __author__ = 'bbowman@pacificbiosciences.com'
 
 from pbmgx.nucmer.coord import NucmerCoordReader
+from pbmgx.nucmer.segment import Segment
 from pbcore.io import FastaReader
 
 MIN_LEN = 10000
@@ -10,28 +11,93 @@ MIN_LEN = 10000
 def calculate_coverage( coord_file, query_fasta, ref_fasta, min_length=MIN_LEN ):
     query_lengths = read_fasta_lengths( query_fasta )
     ref_lengths = read_fasta_lengths( ref_fasta )
-    ref_windows = read_ref_windows( coord_file, min_length )
+    ref_windows = read_ref_segments( coord_file, min_length )
     print ref_windows
-    print query_lengths
-    print ref_lengths
-    combine_windows( ref_windows )
+    segment_dict = combine_segments( ref_windows )
+    gap_dict = find_coverage_gaps( segment_dict, ref_lengths )
+    print segment_dict
+    print gap_dict
+    calculate_coverage_fraction( gap_dict, ref_lengths )
 
-def combine_windows( window_dict ):
-    for reference, windows in window_dict.iteritems():
-        for i, wi in enumerate(windows):
-            for wj in windows[i+1:]:
-                print wi, wj
+def calculate_coverage_fraction( gap_dict, ref_lengths ):
+    for reference, gaps in gap_dict.iteritems():
+        length = float(ref_lengths[reference])
+        covered = [len(g) for g in gaps if g.source == 'covered']
+        uncovered = [len(g) for g in gaps if g.source == 'uncovered']
+        total = covered + uncovered
+        print "Covered Gaps: {0}bp ({1:.2%}) from {2} gaps".format(sum(covered),
+                                                                   sum(covered)/length,
+                                                                   len(covered))
+        print "Uncovered Gaps: {0}bp ({1:.2%}) from {2} gaps".format(sum(uncovered),
+                                                                     sum(uncovered)/length,
+                                                                     len(uncovered))
+        print "Total Gaps: {0}bp ({1:.2%}) from {2} gaps".format(sum(total),
+                                                                 sum(total)/length,
+                                                                 len(total))
 
-def read_ref_windows( coord_file, min_length=MIN_LEN ):
-    windows = {}
+def find_coverage_gaps( segment_dict, lengths ):
+    gap_dict = {}
+    for reference, segments in segment_dict.iteritems():
+        gap_dict[reference] = []
+
+        prev_source = None
+        prev_end = 0
+
+        # Iterate through each Segment, checking for gaps between
+        for segment in segments:
+            if segment.start > prev_end + 1:
+                if segment.source and segment.source == prev_source:
+                    gap = Segment( prev_end+1, segment.start-1, 'covered' )
+                else:
+                    gap = Segment( prev_end+1, segment.start-1, 'uncovered' )
+                gap_dict[reference].append( gap )
+            prev_source = segment.source
+            prev_end = segment.end
+
+        # Check that the end of the segments is flush with the reference end
+        length = lengths[reference]
+        if prev_end < length:
+            gap = Segment( prev_end+1, length, 'uncovered' )
+            gap_dict[reference].append( gap )
+
+    return gap_dict
+
+def combine_segments( segment_dict ):
+    new_segment_dict = {}
+    for reference, segments in segment_dict.iteritems():
+        new_segments = []
+        for i, curr_seg in enumerate( segments ):
+            # Find if curr_seg overlaps existing segments
+            overlap_flag = False
+            for other_seg in new_segments:
+                if curr_seg.overlaps( other_seg ):
+                    overlap_flag = True
+                    break
+            # if so it has already been incorporated and can be skipped
+            if overlap_flag:
+                continue
+
+            # Otherwise, check it against the remaining segments
+            for other_seg in segments[i+1:]:
+                if curr_seg.overlaps( other_seg ) and curr_seg.source == other_seg.source:
+                    curr_seg = curr_seg.combine( other_seg )
+
+            # Add the maximized curr_seg to the output
+            new_segments.append( curr_seg )
+        new_segment_dict[reference] = sorted(new_segments)
+    return new_segment_dict
+
+def read_ref_segments( coord_file, min_length=MIN_LEN ):
+    segments = {}
     for hit in NucmerCoordReader( coord_file ):
-        if abs(hit.S1 - hit.E1) < min_length:
+        segment = Segment( hit.S1, hit.E1, hit.query )
+        if len(segment) < min_length:
             continue
         try:
-            windows[hit.reference].append( (hit.S1, hit.E1) )
+            segments[hit.reference].append( segment )
         except:
-            windows[hit.reference] = [ (hit.S1, hit.E1) ]
-    return windows
+            segments[hit.reference] = [ segment ]
+    return segments
 
 def read_fasta_lengths( fasta_file ):
     lengths = {}
--- src/pbmgx/nucmer/segment.py
+++ src/pbmgx/nucmer/segment.py
@@ -7,16 +7,17 @@ class Segment( object ):
     A Class for representing a segment of a Nucmer Alignment
     """
 
-    def __init__(self, start, end):
+    def __init__(self, start, end, source=None):
         assert isinstance(start, int)
         assert isinstance(end, int)
         assert start != end
         self._start = start
         self._end = end
+        self._source = source
 
-        if self.start > self.end:
+        if self.start < self.end:
             self._forward = True
-        elif self.end > self.start:
+        elif self.start > self.end:
             self._forward = False
 
     @property
@@ -26,6 +27,14 @@ class Segment( object ):
     @property
     def end(self):
         return self._end
+
+    @property
+    def source(self):
+        return self._source
+
+    @property
+    def leftmost(self):
+        return min(self.start, self.end)
 
     @property
     def forward(self):
@@ -42,7 +51,21 @@ class Segment( object ):
         elif self.reverse:
             return 'reverse'
 
-    def overlap(self, other):
+    def __len__(self):
+        return abs(self.start - self.end)
+
+    def __repr__(self):
+        return '<Segment: {0}-{1} from {2}>'.format(self.start, self.end, self.source)
+
+    def __lt__(self, other):
+        assert isinstance(other, Segment)
+        return self.leftmost < other.leftmost
+
+    def __gt__(self, other):
+        assert isinstance(other, Segment)
+        return self.leftmost > other.leftmost
+
+    def overlaps(self, other):
         assert isinstance( other, Segment )
         if self.orientation != other.orientation:
             return False
@@ -54,11 +77,15 @@ class Segment( object ):
 
     def combine(self, other):
         assert isinstance( other, Segment )
-        assert self.overlap( other )
+        assert self.overlaps( other )
         if self.forward:
             start = min( self.start, other.start )
             end = max( self.end, other.end )
         else:
             start = max( self.start, other.start )
             end = min( self.end, other.end )
+
+        # Maintain the Source if shared, otherwise discard
+        if self.source == other.source:
+            return Segment( start, end, self.source )
         return Segment( start, end )
